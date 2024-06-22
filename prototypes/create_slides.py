8a8cb354437e2b5cc2f03a6f41f6c0de9d3b65e6
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2024-06-22 15:24:12 +0000

    Edit slides creation prototype to create a correct demo.

--- prototypes/create_slides.py
+++ prototypes/create_slides.py
@@ -1,9 +1,25 @@
 from moviepy.editor import *
+from datetime import datetime
 import os
 
 session_information = {
-  "Title": "Session Title"
+  "Title": "W1F4-Virtual",
+  "Date": "2024-07-17",
+  "Times": [{
+    "Timezone": "Costa Rica (CST)",
+    "Start": "07:00",
+    "End": "08:10"
+  },
+  {
+    "Timezone": "Miami (EDT)",
+    "Start": "09:00",
+    "End": "10:10"
+  }]
 }
+
+def convert_date_format(date_in: str) -> str:
+  date_out = datetime.strptime(date_in, "%Y-%m-%d")
+  return date_out.strftime("%A, %B %d, %Y")
 
 def get_ratio(original: int, target: int) -> float:
   return target / original
@@ -23,23 +39,42 @@ def create_instructions_clip(
     instruction.set_duration(duration).set_position(('center', 540 + 60))
   ], use_bgclip = True)
 
-def create_banner_clip(banner_img: str) -> VideoClip:
-  clip = ImageClip(banner_img)
-  return clip
+def create_session_clip(
+  duration: int,
+  session_information: dict,
+  banner_img: str
+) -> VideoClip:
+  background = ColorClip(size=(1920, 1080), color=[255, 255, 255])
+  banner_clip = ImageClip(banner_img)
+  banner_clip = banner_clip.resize(get_ratio(banner_clip.size[0], 1880))
+  text = "Session: " + session_information["Title"] + "\n"
+  text += "Date: " + convert_date_format(session_information["Date"]) + "\n\n"
+  for time in session_information["Times"]:
+    text += time["Timezone"] + ": "
+    text += time["Start"] + "-"
+    text += time["End"] + "\n"
+  title = TextClip(text, fontsize=48, color='black', align='center')
+  return CompositeVideoClip([
+    background.set_duration(duration),
+    banner_clip.set_duration(duration).set_position(('center', 20)),
+    title.set_duration(duration).set_position(('center', 540 + 60))
+  ], use_bgclip = True)
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 image_path = os.path.abspath(current_path + '/../sample-data/banner.jpg')
 
-output_path = current_path + '/../sample-data/instructions1.mp4'
 instructions = "Session discussion starting now!\n\n"
 instructions += "Feel free to unmute your microphone\nand turn on your camera."
-video = create_instructions_clip(10, instructions, image_path)
-video.write_videofile(output_path, fps=60)
+video_ins1 = create_instructions_clip(10, instructions, image_path)
 
-output_path = current_path + '/../sample-data/instructions2.mp4'
+video_ins2 = create_session_clip(10, session_information, image_path)
+
 instructions = "Important information.\n\n"
 instructions += "Please keep your microphone muted and camera turned off.\n"
 instructions += "Please use the chat to ask questions, and keep in mind\n"
 instructions += "that the session is being recorded. Enjoy!"
-video = create_instructions_clip(10, instructions, image_path)
-video.write_videofile(output_path, fps=60)
+video_ins3 = create_instructions_clip(10, instructions, image_path)
+
+output_path = current_path + '/../sample-data/session_sample.mp4'
+output_video = concatenate_videoclips([video_ins1, video_ins2, video_ins3], method="compose")
+output_video.write_videofile(output_path, fps=60)
